37685dc7b7bdba8a1c48becfe2f6df9055e02d14
sd-vixen <sonjuatlas@gmail.com>
2025-07-31 13:15:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// This file is currently empty because basic horizontal scrolling
+// is handled purely by HTML and CSS!
 
+// You would add JavaScript here if you wanted more advanced features like:
+// - A lightbox (clicking an image opens a larger version in a popup)
+// - Lazy loading (only loads images when they're about to be seen)
+// - Dynamic image loading (e.g., fetching image paths from a JSON file)
+// - Navigation arrows (to scroll left/right programmatically)
+// - A 'load more' button
